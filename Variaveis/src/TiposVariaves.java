e68553e1a0b2f0d27f5097bd950231d0a9f52685
William Matew <williaw.com@gmail.com>
2025-02-05 01:05:57 -0300

    Criando projeto tipos variaves

---
+++ Variaveis/src/TiposVariaves.java
@@ -0,0 +1,28 @@
+public class TiposVariaves {
+    public static void main(String[] args) {
+        // Tipos primitivos
+        byte idade = 30;
+        short idade2 = 30;
+        int idade3 = 30;
+        long idade4 = 30;
+        float salario = 1000.0f;
+        double salario2 = 1000.0;
+        char letra = 'A';
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        // Tipos não primitivos
+        String nome = "Lucas";
+        System.out.println("Idade: " + idade);
+        System.out.println("Idade2: " + idade2);
+        System.out.println("Idade3: " + idade3);
+        System.out.println("Idade4: " + idade4);
+        System.out.println("Salario: " + salario);
+        System.out.println("Salario2: " + salario2);
+        System.out.println("Letra: " + letra);
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Falso: " + falso);
+        System.out.println("Nome: " + nome);
+    }
+
+}
