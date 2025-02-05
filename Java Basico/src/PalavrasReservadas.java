e54b2132e72d8846f874ad5eabe115800289be50
William Matew <williaw.com@gmail.com>
2025-02-05 18:27:21 -0300

    Palavras Reservadas

---
+++ Java Basico/src/PalavrasReservadas.java
@@ -0,0 +1,25 @@
+//Assuntos:
+
+// tipos primitivos
+// classificações
+// escopo de uso
+// palavra "opostas"
+// contem 52 palavras reservadas
+
+// PALAVRAS "OPOSTAS"
+// CONTROLE DE PACOTES
+// MODIFICADORES DE ACESSO
+// PRIMITIVOS
+// MODIFICADORES DE CLASSES VARIÁVEIS E METODOS
+// CONTROLADORES DE FLUXO
+
+public class PalavrasReservadas {
+    
+//uso 
+
+// arquivos
+// classes
+// metodos
+// atributos
+
+}
