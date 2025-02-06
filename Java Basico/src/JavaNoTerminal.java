f4ac5ee5b57e56acd418a3414581700611e4ab44
William Matew <williaw.com@gmail.com>
2025-02-06 14:08:46 -0300

    Java no terminal

---
+++ Java Basico/src/JavaNoTerminal.java
@@ -0,0 +1,13 @@
+/**
+ * Utilizando java no terminal e com JVM
+ * @author william lira
+ * @since 2025-02-06
+ * @version 1.0
+ */
+
+public class JavaNoTerminal {
+    public static void main(String[] args) {
+        System.out.println("Ola, fui executado no terminal");
+    }
+}
+// para executar no terminal fora da IDE é necessario encontar o arquivo na pasta bin
