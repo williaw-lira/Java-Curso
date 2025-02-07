4e6671beae764a9ba3f61cd8dbb4de850165e8b2
William Matew <williaw.com@gmail.com>
2025-02-07 14:08:02 -0300

    For Arryas

---
+++ Java Basico/src/ForArrays.java
@@ -0,0 +1,22 @@
+// For em Arrays
+
+public class ForArrays {
+    public static void main(String [] args){
+        // Array de inteiros
+        int [] numeros = {1,2,3,5,5,6,5,8,9,10};
+    
+        for(int i = 0; i < numeros.length ;i++ ){
+            System.out.println(numeros[i]);
+        }
+
+        // Array de Strings
+
+        String [] nomes = {"william", "vito", "joao"};
+        for (int x = 0; x < nomes.length; x++){
+            System.out.println(nomes[x]);
+            if (nomes[x].equals("vitor")) {
+                System.out.println("Achei o Vitor");
+            }
+        }
+    }
+}
