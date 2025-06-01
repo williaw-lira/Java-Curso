7f1112b03ff50933df99bda6884168eb38b68ccb
williaw-lira <williaw.com@gmail.com>
2025-06-01 18:57:48 -0300

    "equals"

---
+++ Java Basico/src/Equals.java
@@ -0,0 +1,25 @@
+public class Equals {
+
+/**
+ * @param Equals usamos o equal para comparar textos
+ */
+
+    public static void main(String[] args) {
+        String clima = "chuvoso";
+
+        if (clima.equals("ensolarado")) {
+            System.out.println("Dia perfeito para sair");
+            
+        } else if (clima.equals("nublado")){
+            System.out.println("Otimo dia para sair mas e melhor levar um guarda chuva!");
+        } else{
+            clima.equals("chuvoso");
+            System.out.println("Melhor ficar em casa! ");
+
+            
+        }
+
+
+    }
+    
+}
