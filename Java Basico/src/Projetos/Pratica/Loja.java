f24b7d2fbda2c72dc9bb60c3fa4c493461ef062a
williaw-lira <williaw.com@gmail.com>
2025-06-05 19:31:16 -0300

    construtores

---
+++ Java Basico/src/Projetos/Pratica/Loja.java
@@ -0,0 +1,11 @@
+package Projetos.Pratica;
+
+public class Loja {
+    public static void main(String[] args) {
+        
+        Produtos prod01 = new Produtos("Mouse",199.90F,"infokit");
+    
+        System.out.println("Produto: "+ prod01.getNome() + " Preco: " + prod01.getPreco() + " Marca: " + prod01.getMarca() + " Quantidade: " + prod01.getQuantidade() );
+
+    }
+}
---
+++ Java Basico/src/Projetos/Pratica/Produtos.java
@@ -0,0 +1,38 @@
+package Projetos.Pratica;
+
+public class Produtos {
+    private String nome;
+    private float preco;
+    private String marca;
+    private int quantidade;
+
+        public Produtos(String nome, float preco, String marca){
+        this.nome = nome;
+        this.preco = preco;
+        this.marca = marca;        
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public float getPreco(){
+        return preco;
+    }
+
+
+    public String getMarca(){
+        return marca;
+    }
+
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    
+    public void  setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+}
